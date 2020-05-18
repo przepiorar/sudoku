5049b346d145c4cbf5aa4cd8e09d713f6e4a6392
przepiorar <przepiorar@wp.pl>
2020-05-18 14:27:47 +0200

    wykrywanie dwojek i trojek

---
+++ SudokuZaleznosci/Pomocnicze.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuZaleznosci
+{
+    public class Matrix
+    {
+        private int[,] storage = new int[9, 9];
+
+        public int this[int row, int column]
+        {
+            get { return storage[row, column]; }
+            set { storage[row, column] = value; }
+        }
+        public override string ToString()
+        {
+            string text = "";
+            int i = 0;
+            int j = 0;
+            bool done = true;
+            foreach (var item in storage)
+            {
+                text += item + " ";
+                i++;
+                if (i == 9)
+                {
+                    text += "\n";
+                    i = 0;
+                    j++;
+                    done = false;
+                }
+                else
+                {
+                    if (i % 3 == 0)
+                    {
+                        text += "|";
+                    }
+                }
+                if (j % 3 == 0 && done == false && j != 9)
+                {
+                    text += "___________________\n";
+                    done = true;
+                }
+            }
+            return text;
+        }
+        public string OdczytajLinie(int a)
+        {
+            string text = "";
+            for (int i = 0; i < 9; i++)
+            {
+                text += storage[a, i] + " ";
+                if (i % 3 == 2 && i != 8)
+                {
+                    text += "|";
+                }
+            }
+            return text;
+        }
+    }
+    class Pomocnicze
+    {
+    }
+}
--- SudokuZaleznosci/Program.cs
+++ SudokuZaleznosci/Program.cs
@@ -5,72 +5,15 @@ using System.Text;
 using Sudoku;
 using System.IO;
 
-namespace ConsoleApplication1
+namespace SudokuZaleznosci
 {
     class Program
     {
-        public class Matrix
-        {
-            private int[,] storage = new int[9, 9];
-
-            public int this[int row, int column]
-            {
-                get { return storage[row, column]; }
-                set { storage[row, column] = value; }
-            }
-            public override string ToString()
-            {
-                string text = "";
-                int i = 0;
-                int j = 0;
-                bool done = true;
-                foreach (var item in storage)
-                {
-                    text += item+ " ";
-                    i++;
-                    if (i==9)
-                    {
-                        text += "\n";
-                        i = 0;
-                        j++;
-                        done = false;
-                    }
-                    else
-                    {
-                        if (i%3==0)
-                        {
-                            text += "|";
-                        }
-                    }
-                    if (j%3==0 && done == false && j!=9)
-                    {
-                        text += "___________________\n";
-                        done = true;
-                    }
-                }
-                return text;
-            }
-            public string OdczytajLinie(int a)
-            {
-                string text = "";
-                for (int i = 0; i < 9; i++)
-                {
-                    text += storage[a, i] + " ";
-                    if (i % 3 == 2 && i!=8)
-                    {
-                        text += "|";
-                    }
-                }
-                return text;
-            }
-        }
-
         public static void generuj(int a, SudokuBoard GameBoard, List<Matrix> listMatrix)
         {
             for (int b = 0; b < a; b++)
             {
                 Matrix CellControls = new Matrix();
-
                 GameBoard.Clear();
                 GameBoard.Solver.SolveThePuzzle(UseRandomGenerator: true);
                 for (int i = 0; i < 9; i++)
@@ -80,25 +23,12 @@ namespace ConsoleApplication1
                         CellControls[i, j] = GameBoard.GetCell(9 * i + j).Value;
                     }
                 }
-                Console.WriteLine("plansza " +(b+1) + "\n"+  CellControls.ToString());
+              //  Console.WriteLine("plansza " +(b+1) + "\n"+  CellControls.ToString());
                 listMatrix.Add(CellControls);
             }
         }
 
-        public static int zliczJedynkiNaPoczatku(List<Matrix> SudokuList)
-        {
-
-            int w = 0;
-            for (int i = 0; i < SudokuList.Count; i++)
-            {
-                if (SudokuList[i][0, 0] == 1)
-                    w++;
-
-            }
-            return w;
-        }
-
-        public static List<int[,]> wykryjTrojke(List<Matrix> SudokuList)
+        public static Tuple< List<int[,]>, List<int[,]>> wykryjTrojkiIDwojki(List<Matrix> SudokuList)
         {
             List<int[,]> macierzeZTrojka = new List<int[,]>();
 
@@ -123,6 +53,42 @@ namespace ConsoleApplication1
             MozliweTrojki.Add(new int[3, 2] { { 6, 4 }, { 5, 5 }, { 4, 6 } });
             MozliweTrojki.Add(new int[3, 2] { { 7, 5 }, { 6, 6 }, { 5, 7 } });
 
+
+            List<int[,]> macierzeZDwojka = new List<int[,]>();
+
+            List<int[,]> MozliweDwojki = new List<int[,]>();
+            MozliweDwojki.Add(new int[2, 2] { { 0, 2 }, { 1, 3 }, });
+            MozliweDwojki.Add(new int[2, 2] { { 1, 2 }, { 2, 3 }, });
+            MozliweDwojki.Add(new int[2, 2] { { 2, 2 }, { 3, 3 }, });
+            MozliweDwojki.Add(new int[2, 2] { { 3, 2 }, { 4, 3 }, });
+            MozliweDwojki.Add(new int[2, 2] { { 4, 2 }, { 5, 3 }, });
+            MozliweDwojki.Add(new int[2, 2] { { 5, 2 }, { 6, 3 }, });
+            MozliweDwojki.Add(new int[2, 2] { { 6, 2 }, { 7, 3 }, });
+            MozliweDwojki.Add(new int[2, 2] { { 7, 2 }, { 8, 3 }, });
+
+            MozliweDwojki.Add(new int[2, 2] { { 0, 5 }, { 1, 6 }, });
+            MozliweDwojki.Add(new int[2, 2] { { 1, 5 }, { 2, 6 }, });
+            MozliweDwojki.Add(new int[2, 2] { { 2, 5 }, { 3, 6 }, });
+            MozliweDwojki.Add(new int[2, 2] { { 3, 5 }, { 4, 6 }, });
+            MozliweDwojki.Add(new int[2, 2] { { 4, 5 }, { 5, 6 }, });
+            MozliweDwojki.Add(new int[2, 2] { { 5, 5 }, { 6, 6 }, });
+            MozliweDwojki.Add(new int[2, 2] { { 6, 5 }, { 7, 6 }, });
+            MozliweDwojki.Add(new int[2, 2] { { 7, 5 }, { 8, 6 }, });
+
+            MozliweDwojki.Add(new int[2, 2] { { 2, 0 }, { 3, 1 }, });
+            MozliweDwojki.Add(new int[2, 2] { { 2, 1 }, { 3, 2 }, });
+            MozliweDwojki.Add(new int[2, 2] { { 2, 3 }, { 3, 4 }, });
+            MozliweDwojki.Add(new int[2, 2] { { 2, 4 }, { 3, 5 }, });
+            MozliweDwojki.Add(new int[2, 2] { { 2, 6 }, { 3, 7 }, });
+            MozliweDwojki.Add(new int[2, 2] { { 2, 7 }, { 3, 8 }, });
+
+            MozliweDwojki.Add(new int[2, 2] { { 5, 0 }, { 6, 1 }, });
+            MozliweDwojki.Add(new int[2, 2] { { 5, 1 }, { 6, 2 }, });
+            MozliweDwojki.Add(new int[2, 2] { { 5, 3 }, { 6, 4 }, });
+            MozliweDwojki.Add(new int[2, 2] { { 5, 4 }, { 6, 5 }, });
+            MozliweDwojki.Add(new int[2, 2] { { 5, 6 }, { 6, 7 }, });
+            MozliweDwojki.Add(new int[2, 2] { { 5, 7 }, { 6, 8 }, });
+
             for (int a = 0; a < SudokuList.Count; a++)
             {
                 for (int i = 0; i < MozliweTrojki.Count; i++)
@@ -133,25 +99,46 @@ namespace ConsoleApplication1
                         macierzeZTrojka.Add(new int[1, 2] { { a+1, i+1 } });                       
                     }
                 }
+                for (int i = 0; i < MozliweDwojki.Count; i++)
+                {
+                    if (SudokuList[a][MozliweDwojki[i][0, 0], MozliweDwojki[i][0, 1]] == SudokuList[a][MozliweDwojki[i][1, 0], MozliweDwojki[i][1, 1]])
+                    {
+                        macierzeZDwojka.Add(new int[1, 2] { { a + 1, i + 1 } });
+                    }
+                }
             }
-            return macierzeZTrojka;
+            return new Tuple<List<int[,]>, List<int[,]>>(macierzeZTrojka,macierzeZDwojka) ;
         }
-        static void Main(string[] args)
+
+        public static List<List<int>> wykryjZaleznosci(Tuple<List<int[,]>, List<int[,]>> krotka, int ilosc)
         {
-            // GameBoard Instance
-            SudokuBoard GameBoard = new SudokuBoard();
-            List<Matrix> SudokuList = new List<Matrix>();
-            List<int[,]> lista = new List<int[,]>();
+            List<List<int>> listaZaleznosci = new List<List<int>>(ilosc);
 
-            generuj(20, GameBoard, SudokuList);
-            lista = wykryjTrojke(SudokuList);
-            foreach (int[,] item in lista)
+            for (int i = 1; i < ilosc+1; i++)
             {
-                Console.WriteLine(item[0, 0] + " " + item[0, 1]);
+                listaZaleznosci.Add(new List<int> {i}); //dodanie nr sudoku
             }
-
+            foreach (var item in krotka.Item1)
+            {
+                listaZaleznosci[item[0, 0]-1].Add(item[0, 1]);
+            }
+            foreach (var item in krotka.Item2)
+            {
+                listaZaleznosci[item[0, 0]-1].Add(item[0, 1] * 100);
+            }
+            for (int i = 0; i < listaZaleznosci.Count; i++)
+            {
+                if (listaZaleznosci[i].Count <3)
+                {
+                    listaZaleznosci.RemoveAt(i);
+                    i--;
+                }
+            }
+            return listaZaleznosci;
+        }
+        public static void zapisz(List<Matrix> SudokuList)
+        {
             string FILE_NAME = "Wynik.txt";
-
             StreamWriter sw = new StreamWriter(FILE_NAME);
             for (int i = 0; i < SudokuList.Count; i++)
             {
@@ -162,11 +149,43 @@ namespace ConsoleApplication1
                     if (j % 3 == 2 && j != 9 && j != 0)
                     {
                         sw.WriteLine("___________________");
-
                     }
                 }
             }
             sw.Close();
+        }
+
+        static void Main(string[] args)
+        {
+            SudokuBoard GameBoard = new SudokuBoard();
+            List<Matrix> SudokuList = new List<Matrix>();
+            Tuple<List<int[,]>, List<int[,]>> krotka; //= new Tuple<List<int[,]>, List<int[,]>>(lista, lista2);         
+            int iloscGeneracji = 100;
+            generuj(iloscGeneracji, GameBoard, SudokuList);
+            krotka = wykryjTrojkiIDwojki(SudokuList);
+            zapisz(SudokuList);
+
+            /* tylko wyswietlanie w konsoli */
+            //foreach (int[,] item in krotka.Item1)
+            //{
+            //    Console.WriteLine(item[0, 0] + " " + item[0, 1]);
+            //}
+            //foreach (int[,] item in krotka.Item2)
+            //{
+            //    Console.WriteLine(item[0, 0] + " " + item[0, 1]);
+            //}
+
+            List<List<int>> wynik = new List<List<int>>();
+            wynik = wykryjZaleznosci(krotka, iloscGeneracji);
+            foreach (var item in wynik)
+            {
+                string text = "nr sudoku: " + item[0] + " wykryto: ";
+                for (int i = 1; i < item.Count; i++)
+                {
+                    text += item[i] + " ";
+                }
+                Console.WriteLine(text);
+            }
             Console.ReadKey();
         }
     }
